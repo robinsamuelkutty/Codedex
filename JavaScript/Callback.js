d60646d65297af7ee9ecccb384a53628934d6074
Robin Samuelkutty <114683113+robinsamuelkutty@users.noreply.github.com>
2025-05-28 16:53:04 +0530

    Update Callback.js

--- JavaScript/Callback.js
+++ JavaScript/Callback.js
@@ -6,3 +6,4 @@ subscribeBtn.addEventListener("click" ,()=>{
   message.classList.remove("hidden");
   setTimeout(()=>message.classList.add("hidden"),3000);
 });
+//Callback functions are passed as arguments into a parent function. We don't execute these functions directly. Instead, they are called in the parent function under some conditions
