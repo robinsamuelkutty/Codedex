d75666ee776d50d2ee7cba5c1807ea274b9b6242
Robin Samuelkutty <114683113+robinsamuelkutty@users.noreply.github.com>
2025-05-28 16:52:48 +0530

    Delete JavaScript/Subscribe Button directory

--- JavaScript/Subscribe Button/README.md
+++
@@ -1,1 +0,0 @@
-Callback functions are passed as arguments into a parent function. We don't execute these functions directly. Instead, they are called in the parent function under some conditions
--- JavaScript/Subscribe Button/index.js
+++
@@ -1,8 +0,0 @@
-const subscribeBtn = document.getElementById("subscribeBtn");
-const message = document.getElementById("message");
-
-subscribeBtn.addEventListener("click" ,()=>{
-  subscribeBtn.innerText="Subscribed";
-  message.classList.remove("hidden");
-  setTimeout(()=>message.classList.add("hidden"),3000);
-});
