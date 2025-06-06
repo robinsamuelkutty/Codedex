2b62b992b509fba3d73d69501dedf059c5e8d5b9
Robin Samuelkutty <114683113+robinsamuelkutty@users.noreply.github.com>
2025-06-06 16:40:00 +0530

    Create principleamount.py

---
+++ Python/principleamount.py
@@ -0,0 +1,6 @@
+p= int(input("enter the principle amount="))
+i= float(input("enter the interest rate"))
+n= float(input("enter the no of years"))
+
+si= (p*n*i)/100
+print("simple interest="+str(si))
