7f92a869348f98a1ffd3269df08ea41450d26eaa
Robin Samuelkutty <114683113+robinsamuelkutty@users.noreply.github.com>
2025-06-06 16:27:01 +0530

    Create function.py

---
+++ Python/function.py
@@ -0,0 +1,31 @@
+def sample(num1,num2):
+     sum=num1+num2
+     return sum
+
+res=sample(10,5)
+print(res)
+
+
+
+
+
+
+
+
+
+def hello():
+     a=input("what is your name = ")
+     return a
+
+a=hello()
+
+
+'''def name(a):
+    print(a)
+
+name(a)'''
+
+def hi(a):
+    print("oh! your name is "+a)
+
+hi(a)
