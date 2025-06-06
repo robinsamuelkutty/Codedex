57f33e7779b6270069ac133dc5a2b832372de149
Robin Samuelkutty <114683113+robinsamuelkutty@users.noreply.github.com>
2025-06-06 16:26:39 +0530

    Create arraysum.py

---
+++ Python/arraysum.py
@@ -0,0 +1,9 @@
+n= int(input("enter the limit of the arrays"))
+
+a=[],[]
+print("enter the elements for the array 1=")
+for i in range(0,n):
+    for j in range(0,n):
+        l=int(input())
+        a=[],[].append(l)
+print(a=[])
