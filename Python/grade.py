3e70605fd1a385b7fb125dc226e1098c50f58886
Robin Samuelkutty <114683113+robinsamuelkutty@users.noreply.github.com>
2025-06-06 16:27:21 +0530

    Create grade.py

---
+++ Python/grade.py
@@ -0,0 +1,15 @@
+a= int(input("enter your mark"))
+if a>89 and a<=100 :
+    print("your grade is A")
+elif a>79 :
+    print("your grade is B")
+elif a>69 :
+    print("your grade is C")
+elif a>59 :
+    print("your grade is D")
+elif a>49 :
+    print("your grade is E")
+elif a<50 :
+    print("you are failed")
+else :
+    print("invalid mark")
